296be245613599e7b5c1aa03a8349e344761e6dc
Isiah Malit <isiah.malit@publicissapient.com>
2023-01-16 12:15:33 -0500

    adding ios project

---
+++ PaintTheRoses/PaintTheRosesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaintTheRosesApp.swift
+//  PaintTheRoses
+//
+//  Created by Isiah Marie Ramos Malit on 2023-01-16.
+//
+
+import SwiftUI
+
+@main
+struct PaintTheRosesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
